13eb5ce4c7acb48c776fc0e5b14a81f3f33f5ef3
hyejinlim <yoon1440_@naver.com>
2022-02-13 15:03:21 +0900

    feat: card title, image 추가

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -15,7 +15,7 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         // category
         Container(
@@ -31,7 +31,7 @@ class Body extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20),
                 ),
                 margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                width: MediaQuery.of(context).size.width / 7, // study
+                width: MediaQuery.of(context).size.width / 7.2, // study
                 child: CategoryItem(
                   label: categories[index]['label'],
                   imageSrc: categories[index]['imageSrc'],
@@ -39,7 +39,121 @@ class Body extends StatelessWidget {
               );
             },
           ),
-        )
+        ),
+        /* title */
+
+        /* title */
+        Column(
+          children: [
+            // title
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: Text(
+                      '\'생각의 쓰임\'의 \n 스터디',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  RichText(
+                    text: const TextSpan(
+                      style: TextStyle(color: Colors.grey),
+                      children: [
+                        TextSpan(
+                          text: '전체보기',
+                        ),
+                        WidgetSpan(
+                          child: Icon(
+                            Icons.keyboard_arrow_right,
+                            size: 16,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            // image
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                color: const Color(0xFFFCF587),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Stack(
+                children: [
+                  Center(
+                    child: Image.asset('assets/images/img_book1.png'),
+                  ),
+                  Positioned(
+                    top: 25,
+                    right: 21,
+                    child: Column(
+                      children: [
+                        Image.asset('assets/images/icon_book.png'),
+                        const Text(
+                          '12',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // list
+            // Container(
+            //   height: 300,
+            //   padding: const EdgeInsets.all(10.0),
+            //   child: GridView.count(
+            //     crossAxisCount: 1,
+            //     scrollDirection: Axis.horizontal,
+            //     shrinkWrap: true,
+            //     children: List.generate(4, (index) {
+            //       return Column(
+            //         children: [
+            //           Container(
+            //             padding: const EdgeInsets.all(40),
+            //             margin: const EdgeInsets.all(8),
+            //             child: Text('$index'),
+            //           ),
+            //         ],
+            //       );
+            //     }).toList(),
+            //   ),
+            // ),
+            // Container(
+            //   height: 200,
+            //   decoration: BoxDecoration(
+            //     border: Border.all(color: Colors.red),
+            //   ),
+            //   child: ListView.builder(
+            //     padding: const EdgeInsets.all(10.0),
+            //     scrollDirection: Axis.horizontal,
+            //     itemCount: categories.length,
+            //     itemBuilder: (BuildContext context, int index) {
+            //       return Container(
+            //         width: 300,
+            //         decoration: BoxDecoration(
+            //           border: Border.all(color: Colors.red),
+            //         ),
+            //         margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            //         child: Text('$index'),
+            //       );
+            //     },
+            //   ),
+            // ),
+          ],
+        ),
       ],
     );
   }
