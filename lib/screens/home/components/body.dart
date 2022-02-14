26c06d830dbdf81302c2b378bfd8faebfff40c72
hyejinlim <yoon1440_@naver.com>
2022-02-14 15:09:21 +0900

    fix: card list 2row

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'card_image.dart';
+import 'card_list.dart';
 import 'card_title.dart';
 import 'category_item.dart';
-import 'study_like.dart';
 
 List<Map> categories = [
   {'label': '토론', 'imageSrc': 'assets/images/img_chat.png'},
@@ -15,6 +15,48 @@ List<Map> categories = [
 
 List<Map> cards = [
   {'book': '생각의 쓰임', 'bookImageSrc': 'assets/images/img_book1.png'},
+];
+
+List<Map> studies = [
+  {
+    'iconSrc': 'assets/images/img_icon_blue.png',
+    'name': '주리1',
+    'title': '사소한 일상으로 만드는 컨텐츠1',
+    'datetime': '2021.04.26 ~ 05.21 토 14:00',
+    'peopleCount': 1,
+  },
+  {
+    'iconSrc': 'assets/images/img_icon_yellow.png',
+    'name': '주리2',
+    'title': '사소한 일상으로 만드는 컨텐츠2',
+    'datetime': '2021.04.26 ~ 05.21 토 14:00',
+  },
+  {
+    'iconSrc': 'assets/images/img_icon_blue.png',
+    'name': '주리3',
+    'title': '사소한 일상으로 만드는 컨텐츠1',
+    'datetime': '2021.04.26 ~ 05.21 토 14:00',
+    'peopleCount': 1,
+  },
+  {
+    'iconSrc': 'assets/images/img_icon_yellow.png',
+    'name': '주리4',
+    'title': '사소한 일상으로 만드는 컨텐츠2',
+    'datetime': '2021.04.26 ~ 05.21 토 14:00',
+  },
+  {
+    'iconSrc': 'assets/images/img_icon_yellow.png',
+    'name': '주리7',
+    'title': '사소한 일상으로 만드는 컨텐츠2',
+    'datetime': '2021.04.26 ~ 05.21 토 14:00',
+  },
+  // {
+  //   'iconSrc': 'assets/images/img_icon_blue.png',
+  //   'name': '주리3',
+  //   'title': '사소한 일상으로 만드는 컨텐츠1',
+  //   'datetime': '2021.04.26 ~ 05.21 토 14:00',
+  //   'peopleCount': 1,
+  // },
 ];
 
 class Body extends StatelessWidget {
@@ -57,7 +99,7 @@ class Body extends StatelessWidget {
               likeCount: '12',
             ),
             SizedBox(
-              height: 116,
+              height: 226,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
                 child: Scrollbar(
@@ -66,95 +108,51 @@ class Body extends StatelessWidget {
                   child: ListView.builder(
                     physics: const BouncingScrollPhysics(),
                     controller: scrollController,
-                    // padding: const EdgeInsets.all(10.0),
                     scrollDirection: Axis.horizontal,
-                    itemCount: categories.length,
+                    itemCount: studies.length,
                     itemBuilder: (BuildContext context, int index) {
+                      final i = index;
+                      final iPlus = index + 1;
                       if (index.isOdd) {
                         return const Divider();
                       }
-                      return Container(
-                        width: MediaQuery.of(context).size.width * 0.86, // 345
-                        // decoration: BoxDecoration(
-                        //   border: Border.all(color: Colors.red),
-                        // ),
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 10.0,
-                          vertical: 20.0,
-                        ),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(right: 8),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 6),
-                                    child: Image.asset(
-                                        'assets/images/img_icon_blue.png'),
+                      return (iPlus == studies.length)
+                          ? Column(
+                              children: [
+                                CardList(
+                                  iconSrc: studies[i]['iconSrc'],
+                                  name: studies[i]['name'],
+                                  title: studies[i]['title'],
+                                  datetime: studies[i]['datetime'],
+                                  peopleCount: 4,
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(20.0),
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 10.0,
+                                    vertical: 20.0,
                                   ),
-                                  const Text('주리'),
-                                ],
-                              ),
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(right: 8),
-                                        child: Row(
-                                          children: [
-                                            Image.asset(
-                                              'assets/images/img_book_open_4x.png',
-                                              width: 18,
-                                              height: 18,
-                                            ),
-                                            const Text('1'),
-                                          ],
-                                        ),
-                                      ),
-                                      Row(
-                                        children: [
-                                          Image.asset(
-                                              'assets/images/img_people.png'),
-                                          const Text('3'),
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.only(
-                                      top: 4,
-                                      bottom: 14,
-                                    ),
-                                    child: Text(
-                                      '$index 사소한 일상으로 만드는 컨텐츠',
-                                      style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                  const Text(
-                                    '2021.04.26 ~ 05.21 토 14:00',
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.grey,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            const StudyLikeWidget(),
-                          ],
-                        ),
-                      );
+                                  child: const Text('스터디를 직접 주최해보세요!'),
+                                )
+                              ],
+                            )
+                          : Column(
+                              children: [
+                                CardList(
+                                  iconSrc: studies[i]['iconSrc'],
+                                  name: studies[i]['name'],
+                                  title: studies[i]['title'],
+                                  datetime: studies[i]['datetime'],
+                                  peopleCount: 4,
+                                ),
+                                CardList(
+                                  iconSrc: studies[iPlus]['iconSrc'],
+                                  name: studies[iPlus]['name'],
+                                  title: studies[iPlus]['title'],
+                                  datetime: studies[iPlus]['datetime'],
+                                ),
+                              ],
+                            );
                     },
                   ),
                 ),
