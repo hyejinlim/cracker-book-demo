f12e19ad151a59a7ba0ccf590a9aced2b8274c52
hyejinlim <yoon1440_@naver.com>
2022-02-21 10:50:09 +0900

    fix: category model 적용 및 위젯 분리

--- lib/widgets/category_list.dart
+++ lib/widgets/category_list.dart
@@ -1,5 +1,36 @@
 import 'package:cracker_book_demo/config/constant.dart';
+import 'package:cracker_book_demo/models/category.dart';
 import 'package:flutter/material.dart';
+
+class CategoryList extends StatelessWidget {
+  const CategoryList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      decoration: const BoxDecoration(color: Colors.white),
+      child: ListView.builder(
+        padding: const EdgeInsets.all(10.0),
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 10.0),
+            width: MediaQuery.of(context).size.width / 7.2, // study
+            child: CategoryItem(
+              label: categories[index].label,
+              imageSrc: categories[index].imageSrc,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
 
 class CategoryItem extends StatelessWidget {
   const CategoryItem({
